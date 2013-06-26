b9e31473fe87b8355737e94560974b25c296ee6c
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2013-06-26 19:35:24 +0300

    What about this?

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='django-request-local',
-    version='0.1',
-    description='Make request object available throughout the current thread',
-    author='Proteus Technologies Co. Ltd.',
-    author_email='team@proteus-tech.com',
-    url='https://github.com/Proteus-tech/django-request-local',
-    packages=['django_request_local']
-    )
+  name='django-request-local',
+  version='0.1',
+  description='Make request object available throughout the current thread',
+  author='Proteus Technologies Co. Ltd.',
+  author_email='team@proteus-tech.com',
+  url='https://github.com/Proteus-tech/django-request-local',
+  packages=['django_request_local'],
+  )
